5f85bd8eef8976a4d608adc32167a6f96de8040e
Colin Gillespie <csgillespie@gmail.com>
2018-02-28 14:01:52 +0000

    Annoying merge

--- r-package/src/init.h
+++
@@ -1,9 +0,0 @@
-#ifndef INIT_H_INCLUDED
-#define INIT_H_INCLUDED
-#include "st_main.h"
-
-st_mcmc_npar *initMCMCnPar(int n);
-st_mcmc_1par *initMCMC1Par();
-// st_parallel *init_parallel(int no_threads, int no_d);
-void free_mcmc_n_par(st_mcmc_npar * mcmc_par);
-#endif
